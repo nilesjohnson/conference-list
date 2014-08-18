a260250933c747eb481af1eea8d66aa8ca6a1238
Niles Johnson <nilesj@gmail.com>
2014-08-18 06:57:19 -0600

    broken model tests

--- Test/Case/Model/ConferenceTest.php
+++ Test/Case/Model/ConferenceTest.php
@@ -22,28 +22,34 @@ class ConferenceTest extends CakeTestCase {
  * @return void
  */
 	public function setUp() {
-		parent::setUp();
-		$this->Conference = ClassRegistry::init('Conference');
+	  parent::setUp();
+	  $this->Conference = ClassRegistry::init('Conference');
 	}
 
 
-	/*
+	public function testNone() {
+	  $result = $this->Conference;
+	  $expected = true;
+	  debug($result);
+	  //$this->assertEquals($expected,$result);
+	}
+
 	public function testMultiEmail() {
-	       $result = $this->Conference->multiEmail('em1@host1.com,em2@host2.com, em3@host3.com , em4@host4.com');
-	       $expected = true;
-	       $this->assertEquals($expected,$result);
+	  $result = $this->Conference->multiEmail('em1@host1.com,em2@host2.com, em3@host3.com , em4@host4.com');
+	  $expected = true;
+	  //$this->assertEquals($expected,$result);
 	
 	}	
-	*/
 
 
-	/*
 	public function testBeforeSave() {
+	  $this->Conference->id = 2;
+	  //$this->set('conference', $this->Conference->read());
 	  $result = $this->Conference->beforeSave();
 	  debug($result);
 	  //$this->assertEquals(true,$result);
 	}
-	*/
+
 
 
 /**
