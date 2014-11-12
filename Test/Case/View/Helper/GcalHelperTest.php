d2649775f35c5ab44f83c62ae36b604e049c6caf
Niles Johnson <nilesj@gmail.com>
2014-11-12 07:17:41 -0700

    figured out how to test gcal helper (#18)

---
+++ Test/Case/View/Helper/GcalHelperTest.php
@@ -0,0 +1,20 @@
+<?php
+App::uses('ConferencesController','Controller');
+App::uses('View','View');
+App::uses('GcalHelper','View/Helper');
+
+class GcalHelperTest extends CakeTestCase {
+  public function setUp() {
+    parent::setUp();
+    $Controller = new ConferencesController();
+    $View = new View();
+    $this->gcal = new GcalHelper($View);
+  }
+  public function testGcal() {
+    $expected = 'http://www.google.com/calendar/event?action=TEMPLATE&text=title&dates=20140101/20140202&details=http://url.com&location=city%3B+country&trp=false&sprop=conflist%2Furl&sprop=name:conflist+name';
+    $result = $this->gcal->gcal_url('1','2014-01-01','2014-02-01','title','city','country','http://url.com','conflist/url','conflist name');
+    debug($result);
+    $this->assertEquals($expected,$result);
+  }
+}
+?>
