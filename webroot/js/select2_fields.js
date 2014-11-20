e5a8abc05c17d289e3c82c7e27e0f978cfa23a82
Niles Johnson <nilesj@gmail.com>
2014-11-20 08:37:02 -0700

    update sorting of items in select2 boxes (#19)

--- webroot/js/select2_fields.js
+++ webroot/js/select2_fields.js
@@ -1,14 +1,34 @@
 //this is for all the select2 boxes . . very easy
- $(document).ready(function() {
+$(document).ready(function() {
+ var sortByMatchIndex;
+ $sortByMatchIndex = function(results, container, query) {
+     if (query.term) {
+	 // use the built in javascript sort function
+	 return results.sort(function(a, b) {
+		 return 2*(
+			   a.text.toUpperCase().indexOf(query.term.toUpperCase()) > b.text.toUpperCase().indexOf(query.term.toUpperCase())
+			   )-1;
+	     });
+     }
+     return results;
+ };
+
+
  $("#TagTag").select2({
 	placeholder: "Select subject tags",
 	allowClear: true,
-	width: "100%"
+        width: "100%",
+        sortResults: $sortByMatchIndex
  }); 
  
  $("#ConferenceCountry").select2({
         placeholder: "Country...",
         //allowClear: true,
         width: "100%",
-	matcher: function(term, text) { return text.toUpperCase().indexOf(term.toUpperCase())==0; } }); 
- });
+	matcher: function(term, text) { 
+	     return text.toUpperCase().indexOf(term.toUpperCase())>=0; 
+	},
+	sortResults: $sortByMatchIndex
+
+ }); 
+});
